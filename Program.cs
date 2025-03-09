337675023b3e8158c1ea7f8890d105276659fbb6
awm.builds <81087554+awm-builds@users.noreply.github.com>
2025-03-09 12:03:27 -0400

    Added basic console test. App runs successfully.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Wait for user input before closing the console window
+Console.WriteLine("Press any key to exit...");
+Console.ReadLine();
